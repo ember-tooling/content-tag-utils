1b144bb92c439ce003ee329ee9ae9466bf5da335
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-01-28 18:48:06 -0500

    Remove manual replace templates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 export { extractTemplates } from "./extract-templates.js";
-export { replaceTemplates } from "./replate-templates.js";
 export { transform } from "./transform.js";
 export { coordinatesOf } from "./coordinates-of.js";
 export { reverseInnerCoordinates } from "./reverse-inner-coordinates.js";
